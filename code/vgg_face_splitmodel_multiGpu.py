3b337948562af8dad1ef408c8f83678128eba589
maxgundam <maxgundam@hotmail.com>
2019-07-25 11:44:45 +0800

    daily update

--- code/vgg_face_splitmodel_multiGpu.py
+++ code/vgg_face_splitmodel_multiGpu.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 from glob import glob
-from random import choice, sample
-from myUtils import gen, gen_over_sampling, gen_completely_separated, read_img
+from myUtils import gen, gen_over_sampling, gen_completely_separated, get_a_fold, stratified_k_fold, read_img
 from augmentation import seperation
+from sklearn.externals import joblib
 
-import cv2
+
 import numpy as np
 import pandas as pd
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
@@ -13,67 +13,19 @@ from keras.models import Model
 from keras.optimizers import Adam
 from keras_vggface.utils import preprocess_input
 from keras_vggface.vggface import VGGFace
+from keras.callbacks import TensorBoard
+from keras.utils import multi_gpu_model
 
-basestr = 'splitmodel'
-train_file_path = "../input/train_relationships.csv"
-train_folders_path = "../input/train/"
-val_famillies = "F09"
+import argparse
 
-all_images = glob(train_folders_path + "*/*/*.jpg")
+import tensorflow as tf
+import keras.backend.tensorflow_backend as KTF
 
-train_images = [x for x in all_images if val_famillies not in x]
-val_images = [x for x in all_images if val_famillies in x]
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+session = tf.Session(config=config)
 
-train_person_to_images_map = defaultdict(list)
-
-ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]
-
-for x in train_images:
-    train_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
-
-val_person_to_images_map = defaultdict(list)
-
-for x in val_images:
-    val_person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
-
-relationships = pd.read_csv(train_file_path)
-relationships = list(zip(relationships.p1.values, relationships.p2.values))
-relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
-
-train = [x for x in relationships if val_famillies not in x[0]]
-val = [x for x in relationships if val_famillies in x[0]]
-
-
-# def read_img(path):
-#     img = cv2.imread(path)
-#     img = np.array(img).astype(np.float)
-#     return preprocess_input(img, version=2)
-
-
-# def gen(list_tuples, person_to_images_map, batch_size=16):
-#     ppl = list(person_to_images_map.keys())
-#     while True:
-#         batch_tuples = sample(list_tuples, batch_size // 2)
-#         labels = [1] * len(batch_tuples)
-#         while len(batch_tuples) < batch_size:
-#             p1 = choice(ppl)
-#             p2 = choice(ppl)
-#
-#             if p1 != p2 and (p1, p2) not in list_tuples and (p2, p1) not in list_tuples:
-#                 batch_tuples.append((p1, p2))
-#                 labels.append(0)
-#
-#         for x in batch_tuples:
-#             if not len(person_to_images_map[x[0]]):
-#                 print(x[0])
-#
-#         X1 = [choice(person_to_images_map[x[0]]) for x in batch_tuples]
-#         X1 = np.array([read_img(x) for x in X1])
-#
-#         X2 = [choice(person_to_images_map[x[1]]) for x in batch_tuples]
-#         X2 = np.array([read_img(x) for x in X2])
-#
-#         yield [X1, X2], labels
+KTF.set_session(session)
 
 
 def baseline_model():
@@ -115,6 +67,7 @@ def baseline_model():
 
     model = Model([input_1, input_2], out)
 
+    model = multi_gpu_model(model, gpus=4)
     model.compile(loss="binary_crossentropy", metrics=['acc'], optimizer=Adam(0.00001))  # default 1e-5
 
     model.summary()
@@ -122,59 +75,93 @@ def baseline_model():
     return model
 
 
-file_path = "vgg_face_" + basestr + ".h5"
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="KFold")
+    parser.add_argument('-s', '--SelectedK', default=1)
+    parser.add_argument('-c', '--CreateKF', default=True)
+    parser.add_argument('-k', '--KNumber', default=5)
+    args = parser.parse_args()
 
-checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    basestr = 'KFold'
+    train_file_path = "../input/train_relationships.csv"
+    train_folders_path = "../input/train/"
 
-reduce_on_plateau = ReduceLROnPlateau(monitor="val_acc", mode="max", factor=0.1, patience=20, verbose=1)
+    all_images = glob(train_folders_path + "*/*/*.jpg")
+    person_to_images_map = defaultdict(list)
 
-from keras.callbacks import TensorBoard
-tbCallBack = TensorBoard(log_dir='./logs/'+basestr,
-                         histogram_freq=0,
-                         write_graph=True,
-                         write_images=True)
+    ppl = [x.split("/")[-3] + "/" + x.split("/")[-2] for x in all_images]
+
+    for x in all_images:
+        person_to_images_map[x.split("/")[-3] + "/" + x.split("/")[-2]].append(x)
+
+    relationships = pd.read_csv(train_file_path)
+    relationships = list(zip(relationships.p1.values, relationships.p2.values))
+    relationships = [x for x in relationships if x[0] in ppl and x[1] in ppl]
+    fake_annotation = np.ones(len(relationships))
+    if args.CreateKF == "True":
+        # cos this dataset doesn't have directly annotations, so create a fake one to feed into stratified_k_fold function.
+        print('********************', args.KNumber)
+        kfolds_indices = stratified_k_fold(relationships, fake_annotation, int(args.KNumber))
+        joblib.dump(kfolds_indices, 'kfold.pkl')  # save the folding results
+    else:
+        kfolds_indices = joblib.load('kfold.pkl')
+        data, _ = get_a_fold(relationships, fake_annotation, kfolds_indices, int(args.SelectedK))
+        train = data[0]
+        val = data[1]
+        # print("Training relationship data:", train)
+        # print("Validation relationship data:", val)
+        file_path = "vgg_face_" + basestr + ".h5"
+
+        checkpoint = ModelCheckpoint(file_path, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+
+        reduce_on_plateau = ReduceLROnPlateau(monitor="val_acc", mode="max", factor=0.1, patience=20, verbose=1)
+
+        tbCallBack = TensorBoard(log_dir='./logs/' + basestr,
+                                 histogram_freq=0,
+                                 write_graph=True,
+                                 write_images=True)
+
+        callbacks_list = [checkpoint, reduce_on_plateau, tbCallBack]
+
+        model = baseline_model()
+        print("********encoder's model structure********")
+        print(model.summary())
+        # model.load_weights(file_path)
+        # train_relation_tuple_list = seperation(train, person_to_images_map)
+        model.fit_generator(gen(train, person_to_images_map, batch_size=64),
+                            use_multiprocessing=True,
+                            validation_data=gen(val, person_to_images_map, batch_size=16),
+                            epochs=200,
+                            verbose=1,
+                            workers=8,
+                            callbacks=callbacks_list,
+                            steps_per_epoch=200,
+                            # len(train_relation_tuple_list)//8 + 1,     # len(x_train)//(batch_size) ！！！！！！！！！！！！！
+                            validation_steps=10)
+
+        test_path = "../input/test/"
 
 
-callbacks_list = [checkpoint, reduce_on_plateau, tbCallBack]
-
-model = baseline_model()
-print("********encoder's model structure********")
-print(model.summary())
-# model.load_weights(file_path)
-# train_relation_tuple_list = seperation(train, train_person_to_images_map)
-model.fit_generator(gen(train, train_person_to_images_map, batch_size=16),
-                    use_multiprocessing=True,
-                    validation_data=gen(val, val_person_to_images_map, batch_size=16),
-                    epochs=200,
-                    verbose=2,
-                    workers=4,
-                    callbacks=callbacks_list,
-                    steps_per_epoch=200, # len(train_relation_tuple_list)//8 + 1,     # len(x_train)//(batch_size) ！！！！！！！！！！！！！
-                    validation_steps=10)
-
-test_path = "../input/test/"
+        def chunker(seq, size=32):
+            return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 
-def chunker(seq, size=32):
-    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+        from tqdm import tqdm
 
+        submission = pd.read_csv('../input/sample_submission.csv')
 
-from tqdm import tqdm
+        predictions = []
 
-submission = pd.read_csv('../input/sample_submission.csv')
+        for batch in tqdm(chunker(submission.img_pair.values)):
+            X1 = [x.split("-")[0] for x in batch]
+            X1 = np.array([read_img(test_path + x) for x in X1])
 
-predictions = []
+            X2 = [x.split("-")[1] for x in batch]
+            X2 = np.array([read_img(test_path + x) for x in X2])
 
-for batch in tqdm(chunker(submission.img_pair.values)):
-    X1 = [x.split("-")[0] for x in batch]
-    X1 = np.array([read_img(test_path + x) for x in X1])
+            pred = model.predict([X1, X2]).ravel().tolist()
+            predictions += pred
 
-    X2 = [x.split("-")[1] for x in batch]
-    X2 = np.array([read_img(test_path + x) for x in X2])
+        submission['is_related'] = predictions
 
-    pred = model.predict([X1, X2]).ravel().tolist()
-    predictions += pred
-
-submission['is_related'] = predictions
-
-submission.to_csv("vgg_face_"+basestr+".csv", index=False)
+        submission.to_csv("vgg_face_" + basestr + str(args.SelectedK) + ".csv", index=False)
